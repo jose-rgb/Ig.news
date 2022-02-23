4f165d8d048ac379a2e4acd8ab453b6e7b3c01da
jose-rgb <josericardosaraiva7@gmail.com>
2022-02-23 20:06:13 -0300

    salvando user no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
 
@@ -16,4 +19,42 @@ export default NextAuth({
     }),
   ],
 
+  callbacks: {
+    async signIn({user, account, profile}){
+      const { email } = user
+
+      //try para retornar se login deu certo
+      try {
+        //FQL
+        await fauna.query(
+          //se user n existe 
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            //criar
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            // se ele existe, buscar 
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+        return true
+      } catch {
+        return false
+      } 
+    }
+  }
 })
--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -1,5 +1,7 @@
 import { Client } from 'faunadb';
 
 export const fauna = new Client({
-    secret: process.env.FAUNADB_KEY
+    secret: process.env.FAUNADB_KEY,
+    // config usa
+    domain: 'db.us.fauna.com',
 })
